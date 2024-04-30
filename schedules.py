73ed277b432c5a05edc0d9e54dd5b10d2d8984e6
RA4Z <rraz639@gmail.com>
2024-04-30 16:15:17 -0300

    Adicionando agendamento em arquivos de rede

--- schedules.py
+++ schedules.py
@@ -1,12 +1,33 @@
 import schedule
 import time
+import os
+import subprocess
 from services.functions import *
-
-schedule.every().day.at('08:46:00').do(encerramento_ordens)
-schedule.every().day.at('03:55:00').do(coletar_materiais_ecm)
-schedule.every().day.at('16:19').do(indicador_expedicao)
 
 def run_scheduler():
     while True:
         schedule.run_pending()
         time.sleep(1)
+
+def check_files():
+    path = 'Q:\GROUPS\BR_SC_JGS_WM_LOGISTICA\PCP\Central\__Automaticos_Python\__Agendamentos'
+    files = os.listdir(path)
+
+    schedule.clear()
+    for file in files:
+        filename = file.replace('.txt','')
+        filename = filename.replace('.',':')
+        schedule.every().day.at(filename).do(run_schedule_today)
+
+    print(schedule.jobs)
+        
+
+def run_schedule_today():
+    now = time.time()
+    now = time.strftime("%H.%M", time.localtime(now))
+    filename = open(f'Q:\GROUPS\BR_SC_JGS_WM_LOGISTICA\PCP\Central\__Automaticos_Python\__Agendamentos/{now}.txt','r',encoding='utf-8').readline().strip()
+    print(f'ABRINDO O ARQUIVO {filename}')
+    subprocess.run(['python', filename], check=True)
+
+
+schedule.every(1).minutes.do(check_files)
