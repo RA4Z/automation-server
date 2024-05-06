c65f172c6f78cd15e379abb14a942153fd74df23
RA4Z <rraz639@gmail.com>
2024-05-06 12:44:14 -0300

    Alterando forma do agendamento

--- schedules.py
+++ schedules.py
@@ -1,33 +1,63 @@
-import schedule
 import time
 import os
 import subprocess
-from services.functions import *
 
 def run_scheduler():
+    queue = []
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        try:
+            queue = check_files(queue)
+            status = something_is_running(queue)
 
-def check_files():
+            if not status:
+                now = time.time()
+                now = time.strftime("%H:%M", time.localtime(now))
+
+                for schedule in queue:
+                    if schedule['time'] == now and not schedule['performed']:
+                        print(schedule)
+                        schedule['performed'] = True
+                        subprocess.run(['python', schedule['filename']])
+
+        except Exception as e:
+            print(f'Error: {str(e)}')
+        
+        time.sleep(5)
+
+def check_files(param_queue:list):
     path = 'Q:\GROUPS\BR_SC_JGS_WM_LOGISTICA\PCP\Central\__Automaticos_Python\__Agendamentos'
     files = os.listdir(path)
 
-    schedule.clear()
+    queue = param_queue
     for file in files:
-        filename = file.replace('.txt','')
-        filename = filename.replace('.',':')
-        schedule.every().day.at(filename).do(run_schedule_today)
+        time = file.replace('.txt','')
+        time = time.replace('.',':')
 
-    print(schedule.jobs)
-        
+        if time not in [item['time'] for item in queue]:
+            filename = open(f'{path}/{file}','r',encoding='utf-8').readline().strip()
+            queue.append({'time':time, 'filename':filename,'performed':False})
+    
+    return queue
 
-def run_schedule_today():
-    now = time.time()
-    now = time.strftime("%H.%M", time.localtime(now))
-    filename = open(f'Q:\GROUPS\BR_SC_JGS_WM_LOGISTICA\PCP\Central\__Automaticos_Python\__Agendamentos/{now}.txt','r',encoding='utf-8').readline().strip()
-    print(f'ABRINDO O ARQUIVO {filename}')
-    subprocess.run(['python', filename], check=True)
+def something_is_running(queue:list):
+    for schedule in queue:
+        file = get_last_delimiter_segment(schedule['filename'])
+        folder = str(schedule['filename']).replace(file,'')
+        try:
+            if os.path.exists(f'{folder}status.txt'):
+                with open(f'{folder}status.txt', 'r', encoding='utf-8') as file:
+                    file.readline().strip()
+                    status = file.readline().strip()
+                    if status == 'True': return True
+        except:
+            pass
+    
+    return False
 
 
-schedule.every(1).minutes.do(check_files)
+def get_last_delimiter_segment(text):
+    for delimiter in ["/", "\\"]:
+        index = text.rfind(delimiter)
+        if index != -1:
+            return text[index + 1 :]
+    return text
